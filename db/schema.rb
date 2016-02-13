784d405daa50e69c7173254d88baded067782328
Michael Palaiologos <Palaiologos1990@gmail.com>
2016-02-13 13:07:31 -0800

    Add mailer for flight booking

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213205410) do
+ActiveRecord::Schema.define(version: 20160213205759) do
 
   create_table "airports", force: :cascade do |t|
     t.string   "code"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20160213205410) do
     t.float    "price"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "passenger_bookings", force: :cascade do |t|
+    t.integer  "passenger_id"
+    t.integer  "booking_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "passengers", force: :cascade do |t|
